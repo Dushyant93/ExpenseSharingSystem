ba7ab19f7f764241ed2fae44877d29abc71d5f22
Hashim Hilal <hashim.hilal@connect.qut.edu.au>
2026-05-10 14:06:36 +1000

    feat: add profile routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,69 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
+
+// Auth pages
+import Login    from './pages/Login';
 import Register from './pages/Register';
-import Profile from './pages/Profile';
+
+// Main pages
 import Dashboard from './pages/Dashboard';
 
+// Expense pages
 import ExpenseList from './pages/expenses/ExpenseList';
 import AddExpense  from './pages/expenses/AddExpense';
 import EditExpense from './pages/expenses/EditExpense';
 
+// Group pages
 import GroupList from './pages/groups/GroupList';
 import AddGroup  from './pages/groups/AddGroup';
 import EditGroup from './pages/groups/EditGroup';
+
+// Settlement pages
+import BalanceDashboard from './pages/settlements/BalanceDashboard';
+import AddSettlement    from './pages/settlements/AddSettlement';
+import SettlementList from './pages/settlements/SettlementList';
+
+// Notification page
+import Notifications from './pages/notifications/Notifications';
+
+// Profile pages
+import Profile        from './pages/profile/Profile';
+import EditProfile    from './pages/profile/EditProfile';
+import ChangePassword from './pages/profile/ChangePassword';
 
 import './index.css';
 
 function App() {
   return (
     <Router>
-      backend/middleware/authMiddleware.js
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/expenses" element={<ExpenseList />} />
-        <Route path="/expenses/add" element={<AddExpense />} />
+        <Route path="/"          element={<Navigate to="/dashboard" replace />} />
+        <Route path="/login"     element={<Login />} />
+        <Route path="/register"  element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Expense routes */}
+        <Route path="/expenses"          element={<ExpenseList />} />
+        <Route path="/expenses/add"      element={<AddExpense />} />
         <Route path="/expenses/edit/:id" element={<EditExpense />} />
 
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard" element={ <Dashboard /> } />
-        
-        <Route path="/groups" element={ <GroupList /> } />
-        <Route path="/groups/add" element={ <AddGroup /> } />
-        <Route path="/groups/edit/:id" element={ <EditGroup /> } />
+        {/* Group routes */}
+        <Route path="/groups"          element={<GroupList />} />
+        <Route path="/groups/add"      element={<AddGroup />} />
+        <Route path="/groups/edit/:id" element={<EditGroup />} />
 
-        {/* redirect unknown URLs to dashboard */}
+        {/* NEW: Settlement routes */}
+        <Route path="/settlements/balances/:groupId" element={<BalanceDashboard />} />
+        <Route path="/settlements/add/:groupId"      element={<AddSettlement />} />
+        <Route path="/settlements/:groupId" element={<SettlementList />} />
+
+        {/* NEW: Notification route */}
+        <Route path="/notifications" element={<Notifications />} />
+
+        {/* NEW: Profile routes */}
+        <Route path="/profile"                 element={<Profile />} />
+        <Route path="/profile/edit"            element={<EditProfile />} />
+        <Route path="/profile/change-password" element={<ChangePassword />} />
+
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </Router>
